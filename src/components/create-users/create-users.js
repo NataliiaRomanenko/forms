368f5a74a9b52b223f0c4bccaa3cde846c496110
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-10-15 10:30:05 +0300

    styled create users

--- src/components/create-users/create-users.js
+++ src/components/create-users/create-users.js
@@ -46,64 +46,72 @@ class CreateUser extends Component{
         return(
             <div className="blockWrapper column">
                 <NavLink to={'/home'} className="pagination">Home</NavLink>
-                <p className='title'>Let's create users</p>
-                <form className=" form createUser">
-                    <div className="container">
-                        <div className="group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" name="username" required onChange={this.handleChange}/>
-                            <span className="highlight"/>
-                            <span className="bar"/>
-                        </div>
-                        <fieldset> <legend>User's rights</legend>
-                            <label><input type="radio" name="isAdmin" value='true'  onChange={this.handleChange}/>admin</label>
-                            <label><input type="radio" name="isAdmin" value='false' onChange={this.handleChange}/>user</label>
-                        </fieldset>
+                <div className="flexWrapp">
+                    <div className = "leftColumn">
+                        <p className='title'>Let's create users</p>
+                        <form className=" form createUser">
+                            <div className="container">
+                                <div className="group">
+                                    <label htmlFor="username">Username</label>
+                                    <input type="text" name="username" required onChange={this.handleChange}/>
+                                    <span className="highlight"/>
+                                    <span className="bar"/>
+                                </div>
+                                <fieldset> <legend>User's rights</legend>
+                                    <label><input type="radio" name="isAdmin" value='true'  onChange={this.handleChange}/>admin</label>
+                                    <label><input type="radio" name="isAdmin" value='false' onChange={this.handleChange}/>user</label>
+                                </fieldset>
+                            </div>
+                            <div className="container buttonGroup">
+                                <button type="button" onClick={()=> {
+                                    if(!username && !isAdmin) return;
+                                    addNewUser({username:username,isAdmin:isAdmin});
+                                    this.setToDef();
+                                }}>Create User</button>
+
+                            </div>
+                        </form>
                     </div>
-                    <div className="container buttonGroup">
-                        <button type="button" onClick={()=> {
-                            if(!username && !isAdmin) return;
-                            addNewUser({username:username,isAdmin:isAdmin});
-                            this.setToDef();
-                        }}>Create User</button>
+                    <div className="rightColumn">
+                        {
+                            newUsers.length ?
+                                <div className="tableWrapper">
+                                    <p className='title'><span className="userCount">{newUsers.length}</span> Users</p>
+                                    <table>
+                                        <thead>
+                                        <tr>
+                                            <th>User name</th>
+                                            <th>User rights</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {
+                                            newUsers.map((user, i) => {
+                                                    return <tr key={i}>
+                                                        <td>{user.username}</td>
+                                                        {
+                                                            user.isAdmin ?
+                                                                <td>user is Admin</td>
+                                                                :
+                                                                <td>user is User</td>
+                                                        }
 
+                                                    </tr>
+                                                }
+
+
+                                            )
+                                        }
+                                        </tbody>
+
+                                    </table>
+                                </div>
+                                :""
+                        }
                     </div>
-                </form>
-                {
-                    newUsers.length ?
-                        <div className="tableWrapper">
-                            <p className='title'><span className="userCount">{newUsers.length}</span> Users</p>
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>User name</th>
-                                    <th>User rights</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {
-                                    newUsers.map((user, i) => {
-                                       return <tr key={i}>
-                                           <td>{user.username}</td>
-                                           {
-                                               user.isAdmin ?
-                                                   <td>user is Admin</td>
-                                                   :
-                                                   <td>user is User</td>
-                                           }
-
-                                       </tr>
-                                    }
+                </div>
 
 
-                                )
-                                }
-                                </tbody>
-
-                            </table>
-                        </div>
-                    :""
-                }
 
             </div>
         )
